85487ee870780c410ca4c58cc5149468b4389d98
Eka Bernard <bernard.eka@gmail.com>
2021-02-15 16:46:44 +0700

    Hitung Modular, Hitung Geometri dan Package requests

--- use_requests.py
+++ use_requests.py
@@ -21,8 +21,10 @@ print('ATAU')
 url = 'https://detik.com'
 try:
     response = requests.get(url)
-    print(f'Success! {response}')
-    print(f'content {response.text}')
-
+    if response.status_code == 200 :
+        print(f'Success! Response = {response.status_code}')
+        print(f'content {response.text}')
+    else :
+        print(f'ups, ada kesalahan request {response.status_code}')
 except Exception as e:
     print(f'There is an error {e}')
