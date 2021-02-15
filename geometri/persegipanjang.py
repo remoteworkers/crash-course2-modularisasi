ee43dd43f30ac5698dcd7d133a0ef05fd9a72c2f
Eka Bernard <bernard.eka@gmail.com>
2021-02-15 23:35:45 +0700

    Hitung Modular, Hitung Geometri dan Package requests

--- geometri/persegipanjang.py
+++ geometri/persegipanjang.py
@@ -1,4 +1,9 @@
 print('\nMembuat fungsi hitung_luas_persegi panjang')
+
+def info() :
+    return 'Modul menghitung persegi panjang'
+
 def hitung_luas_persegi_panjang(panjang,lebar):
     luas_persegi_panjang = panjang * lebar
     return luas_persegi_panjang
+
--- geometri/segitiga.py
+++ geometri/segitiga.py
@@ -1,4 +1,8 @@
 print('\nMembuat fungsi hitung_luas_segitiga')
+
+def info():
+    return "Modul menghitung segitiga"
+
 def hitung_luas_segitiga(alas,tinggi):
     luas_segitiga = alas * tinggi / 2
     return luas_segitiga
